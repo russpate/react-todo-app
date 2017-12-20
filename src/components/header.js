3c589972fafc92e22d1a2e06f09168a024dc7e69
Russell Pate <russ@russpate.com>
2017-12-19 20:53:27 -0500

    added header, input, and some style

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Header extends React.Component{
+    render(){
+        return(
+            <div className="wrapper">
+                <div className="wrapper--inner">
+                <h1>To Do App</h1>
+                </div>
+            </div>
+        );
+    }
+}
