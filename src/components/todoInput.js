44c479fff2e5ef6688ae890d614d57854fdd264b
Russell Pate <russ@russpate.com>
2017-12-19 21:10:29 -0500

    adding elements to input component

--- src/components/todoInput.js
+++ src/components/todoInput.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 export default class TodoInput extends React.Components{
+    constructor(props){
+        super(props)
+    }
     render(){
         return(
-
+            <div>
+                <input type="text" value="" onChange={this.handleChange} />
+                <button className="button" onClick={() => this.addToDo(this.state.value)}>Submit<button />
+            </div>
         );
     }
 }
